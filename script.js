b39c0f4e00f8e8d8be7a9489b072acb7d00bd0e2
Kido <kido@gmail.com>
2025-08-31 10:57:41 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Typed animation
+var typed = new Typed("#typed", {
+    strings: ["Full-Stack Web Developer","Database Developer","UI/UX Designer"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 3000,
+    showCursor: false,
+    loop: true
+});
+
+// Theme
+function theme() {
+    const darkBtn = document.getElementById('darkBtn');
+    const lightBtn = document.getElementById('lightBtn');
+
+    document.body.classList.toggle('light');
+
+    if(document.body.classList.contains('light')) {
+        darkBtn.style.display = 'none';
+        lightBtn.style.display = 'block';
+    } else {
+        darkBtn.style.display = 'block';
+        lightBtn.style.display = 'none';
+    }
+}
